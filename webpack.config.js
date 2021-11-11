d4962af35b06c6ae6edce01789451a53846fdb4b
jeodom17 <jeodom17@gmail.com>
2021-11-11 18:34:08 -0500

    create const config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,31 @@
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 
+const config = {
+    entry: './public/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: __dirname + '/public/dist'
+    },
+
+mode: 'production',
+module: {
+    rules: [
+        {
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                },
+            },
+        },
+    ],
+},
+//* add plug-ins below
+plugins: []
+
+  };
 
 
 
