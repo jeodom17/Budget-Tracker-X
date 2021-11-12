169c1326858d08c668d0fa46e5bf17a440bfb8d9
jeodom17 <jeodom17@gmail.com>
2021-11-11 19:12:11 -0500

    complete webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,30 @@ module: {
     ],
 },
 //* add plug-ins below
-plugins: []
+plugins: [
+    new WebpackPwaManifest({
+        filename: 'manifest.json',
+        fingerprints: false,
+        inject: false,
+        name: 'Budget App',
+        short_name: 'Budget App',
+        description: 'A budget tracking app that allows you to track your withdrawals and deposits with or without a data/internet connection',
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+            {
+                src: path.resolve(__dirname, 'public/icons/icon-512x512.png'),
+                sizes: [72, 96, 128, 192, 256, 384, 512],
+
+            }
+        ]
+    })
+]
 
   };
 
-
-
-
+//* export config
 
 module.exports = config;
